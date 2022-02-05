8a52f249a58b6e7e9faac3188fe0d7d3e4a24e0e
Tim Bryan <tim.bryan@drpgroup.com>
2022-02-05 21:27:44 +0000

    add CreditsModal

---
+++ components/modals/credits.tsx
@@ -0,0 +1,26 @@
+import Anchor from '../anchor'
+import Modal from './modal'
+
+const CreditsModal: React.FC = () => (
+  <Modal label='credits' title='Credits'>
+    <p>
+      Developed by{' '}
+      <Anchor
+        href='https://timbryan.dev'
+      >
+        @TimBryanDev
+      </Anchor>
+    </p>
+    <p className='mt-5 text-left'>Inspired by my wife, who really enjoys the game mechanics of the original game this is based on.</p>
+    <p className='mt-5 text-left'>Technologies:</p>
+    <ul className='list-disc text-left p-5'>
+      <li><Anchor href='https://www.npmjs.com/package/@genzou/random-words'>@genzou/random-words</Anchor> -  for random word generation</li>
+      <li><Anchor href='https://nextjs.org/'>NEXT.JS</Anchor></li>
+      <li><Anchor href='https://tailwindcss.com'>tailwindcss</Anchor></li>
+      <li><Anchor href='https://www.typescriptlang.org/'>TypeScript</Anchor></li>
+      <li><Anchor href='https://vercel.com/'>Vercel</Anchor> - for deployments and hosting</li>
+    </ul>
+  </Modal>
+)
+
+export default CreditsModal
