935681425e01eae41ae5d8df49e4cf5a8801f663
Tim Bryan <tim.bryan@drpgroup.com>
2022-02-09 20:11:20 +0000

    create state gameState and use it

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,20 @@ import Form from '../components/form'
 import GameOver from '../components/gameOver'
 import Guesses from '../components/guesses'
 import Header from '../components/header/header'
-import { GUESS_LIMIT } from '../config/consts'
+import {
+  GUESS_LIMIT,
+  GAME_STATE_INPROGRESS,
+  GAME_STATE_LOSE,
+  GAME_STATE_WIN
+} from '../config/consts'
 
 // @ts-expect-error no type definitions for third-party library :/
 import randomWords from '@genzou/random-words'
+
+type GameState =
+  | typeof GAME_STATE_INPROGRESS
+  | typeof GAME_STATE_LOSE
+  | typeof GAME_STATE_WIN
 
 export const getAnswer = (): string => {
   const words = randomWords({ exactly: 1, maxLength: 5, minLength: 5 })
@@ -22,10 +32,7 @@ const Home: NextPage = () => {
   const { dispatch } = useGuessContext()
   const [guesses, setGuesses] = useState<string[]>([])
   const [answer, setAnswer] = useState<string>(getAnswer())
-
-  const isWinner = guesses.length > 0 && guesses[guesses.length - 1] === answer
-  const isLoser =
-    guesses.length >= GUESS_LIMIT && guesses[guesses.length - 1] !== answer
+  const [gameState, setGameState] = useState<GameState>(GAME_STATE_INPROGRESS)
 
   const resetGame = (): void => {
     setAnswer(getAnswer())
@@ -53,29 +60,42 @@ const Home: NextPage = () => {
     dispatch({ type: 'SET_LETTERS', payload: letterStatuses })
   }, [answer, dispatch, guesses])
 
+  // calculate game state
+  useEffect(() => {
+    if (guesses[guesses.length - 1] === answer) {
+      setGameState(GAME_STATE_WIN)
+      return
+    }
+
+    if (guesses.length >= GUESS_LIMIT && guesses[guesses.length - 1] !== answer) {
+      setGameState(GAME_STATE_LOSE)
+      return
+    }
+
+    setGameState(GAME_STATE_INPROGRESS)
+  }, [answer, guesses])
+
   return (
     <div className='wrapper w-screen h-screen overflow-auto bg-teal-100'>
       <Header />
       <div className='max-w-screen-sm m-auto grid place-items-center my-16'>
         <Description />
-        {isWinner
-          ? (
-            <GameOver guesses={guesses} answer={answer} onContinue={resetGame}>
-              <span className='text-green-500'>You win!</span>
-            </GameOver>
-            )
-          : isLoser
-            ? (
-              <GameOver guesses={guesses} answer={answer} onContinue={resetGame}>
-                <span className='text-red-500'>You lose!</span>
-              </GameOver>
-              )
-            : (
-              <>
-                <Guesses guesses={guesses} answer={answer} />
-                <Form guesses={guesses} setGuesses={setGuesses} />
-              </>
-              )}
+        {gameState === GAME_STATE_WIN && (
+          <GameOver guesses={guesses} answer={answer} onContinue={resetGame}>
+            <span className='text-green-500'>You win!</span>
+          </GameOver>
+        )}
+        {gameState === GAME_STATE_LOSE && (
+          <GameOver guesses={guesses} answer={answer} onContinue={resetGame}>
+            <span className='text-red-500'>You lose!</span>
+          </GameOver>
+        )}
+        {gameState === GAME_STATE_INPROGRESS && (
+          <>
+            <Guesses guesses={guesses} answer={answer} />
+            <Form guesses={guesses} setGuesses={setGuesses} />
+          </>
+        )}
         <ModalManager />
       </div>
     </div>
