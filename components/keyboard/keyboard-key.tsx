fdff83fe11d2f9ab989c42ce1321b1a1e0dd624b
Tim Bryan <tim.bryan@drpgroup.com>
2022-02-11 15:27:26 +0000

    [fix] restore enter and delete key styles

--- components/keyboard/keyboard-key.tsx
+++ components/keyboard/keyboard-key.tsx
@@ -9,8 +9,8 @@ const KeyboardKey = ({
 }): JSX.Element => (
   <button
     key={`keyboard__key--${letter}`}
-    className={`keyboard__key flex-1 p-1 sm:m-1 border border-gray-400 rounded ${
-      styles.includes('bg-') ? styles : 'bg-white'
+    className={`keyboard__key flex-1 p-1 sm:m-1 border border-gray-400 rounded ${styles} ${
+      !styles.includes('bg-') ? 'bg-white' : ''
     }`}
     name={letter}
     onClick={evt => {
