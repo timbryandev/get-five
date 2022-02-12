a24e914beb0974dd441c762fe35d6cfcdbc55ca8
TimBryan <hi@timbryan.dev>
2022-02-12 13:42:29 +0000

    [fix] allow keyboard margin on small screens

--- components/keyboard/keyboard-key.tsx
+++ components/keyboard/keyboard-key.tsx
@@ -9,7 +9,7 @@ const KeyboardKey = ({
 }): JSX.Element => (
   <button
     key={`keyboard__key--${letter}`}
-    className={`keyboard__key flex-1 p-1 sm:m-1 border border-gray-400 rounded ${styles} ${
+    className={`keyboard__key flex-1 p-1 m-1 border border-gray-400 rounded ${styles} ${
       !styles.includes('bg-') ? 'bg-white' : ''
     }`}
     name={letter}
