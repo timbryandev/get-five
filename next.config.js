c5f330ed139ddd11d5707c58d404983f7225a36e
Tim Bryan <tim.bryan@drpgroup.com>
2022-02-15 16:39:37 +0000

    set language to en-GB

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  i18n: {
+    locales: ['en-GB'],
+    defaultLocale: 'en-GB'
+  }
 }
 
 module.exports = nextConfig
